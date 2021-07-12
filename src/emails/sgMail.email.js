319e410cb985d8587f0ff17cd0b63c9c7e357647
Pham Tuan <phamtuan199911@gmail.com>
2021-07-12 10:14:45 +0700

    Add error logger

--- src/emails/sgMail.email.js
+++ src/emails/sgMail.email.js
@@ -1,6 +1,5 @@
 const sgMail = require('@sendgrid/mail');
 
-console.log(`SG API KEY: ${process.env.SENDGRID_API_KEY}`);
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 module.exports = sgMail;
--- src/routes/lecture.route.js
+++ src/routes/lecture.route.js
@@ -118,6 +118,7 @@ lectureRoute.post('/lectures/staging', authentication, rolesValidation(['Admin',
       uploadUrl
     });
   } catch (error) {
+    console.log(error)
     res.status(500).send({
       error: 'Internal Server Error',
     });
